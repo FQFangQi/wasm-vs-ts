17ed046c5c07df4afb995910d9449926bc7427e1
FQFangQi <1786877538@qq.com>
2025-11-11 13:24:54 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wasm-vs-ts/', // 添加这一行，仓库名称
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
